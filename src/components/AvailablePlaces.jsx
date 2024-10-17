d4f225303b7a6a0ab93c0f643626ef43927e15c3
Amato Luca <amatoroccoluca@gmail.com>
2024-10-17 19:32:15 +0200

    chore: create Error component to manage Error with fetch actions

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,23 +1,54 @@
 import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
+import ErrorMessage from '../Error.jsx';
+import { sortPlacesByDistance } from '../loc.js';
+import { fetchAvailablePlaces } from '../http.js';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+  const [errorState, setErrorState] = useState();
 
   useEffect(() => {
     const fetchPlaces = async () => {
-      const response = await fetch('http://localhost:3000/places');
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+      setIsFetching(true);
+
+      try {
+        const places = await fetchAvailablePlaces();
+
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
+      } catch (error) {
+        setErrorState({
+          message:
+            error.message || 'Could not fetch places, please try again later.',
+        });
+        setIsFetching(false);
+      }
     };
 
     fetchPlaces();
   }, []);
 
+  if (errorState) {
+    return (
+      <ErrorMessage title={'An error occurred!'} message={errorState.message} />
+    );
+  }
+
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText={'Fetching place data...'}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
